6efc3a273f6c46c96de7a7595688b040a025bdb7
sigit wahyu <sigitwahyu05@gmail.com>
2019-11-08 15:19:28 +0700

    penambahan bo imp

---
+++ src/com/neurix/simrs/transaksi/checkup/bo/CheckupBo.java
@@ -0,0 +1,7 @@
+package com.neurix.simrs.transaksi.checkup.bo;
+
+/**
+ * Created by Toshiba on 08/11/2019.
+ */
+public interface CheckupBo {
+}
---
+++ src/com/neurix/simrs/transaksi/checkup/bo/impl/CheckupBoImpl.java
@@ -0,0 +1,10 @@
+package com.neurix.simrs.transaksi.checkup.bo.impl;
+
+import com.neurix.simrs.transaksi.checkup.bo.CheckupBo;
+
+/**
+ * Created by Toshiba on 08/11/2019.
+ */
+public class CheckupBoImpl implements CheckupBo {
+
+}
