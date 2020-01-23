7a43aab65c19af47878fe6b2ad6758426f6d700e
Sigit Wahyu Wibowo <sigitwahyu05@gmail.com>
2020-01-23 18:13:47 +0700

    perbaikan

---
+++ src/com/neurix/simrs/transaksi/hargaobat/dao/HargaObatDao.java
@@ -0,0 +1,7 @@
+package com.neurix.simrs.transaksi.hargaobat.dao;
+
+/**
+ * Created by reza on 23/01/20.
+ */
+public class HargaObatDao {
+}
---
+++ src/com/neurix/simrs/transaksi/hargaobat/model/HargaObat.java
@@ -0,0 +1,7 @@
+package com.neurix.simrs.transaksi.hargaobat.model;
+
+/**
+ * Created by reza on 23/01/20.
+ */
+public class HargaObat {
+}
