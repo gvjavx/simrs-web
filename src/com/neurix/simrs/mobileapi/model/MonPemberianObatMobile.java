13c11f6499471f5bf7c60d11b50d811df7bdff18
Mohammad Chaliffilardhy Syaifuddin <ardhymcs@gmail.com>
2020-02-15 10:04:41 +0700

    review

---
+++ src/com/neurix/simrs/mobileapi/model/MonPemberianObatMobile.java
@@ -0,0 +1,163 @@
+package com.neurix.simrs.mobileapi.model;
+
+import java.sql.Timestamp;
+
+/**
+ * @author gondok
+ * Saturday, 15/02/20 9:30
+ */
+public class MonPemberianObatMobile {
+    private String id;
+    private String namaObat;
+    private String dosis;
+    private String caraPemberian;
+    private String skinTes;
+    private String waktu;
+    private String keterangan;
+    private String noCheckup;
+    private String idDetailCheckup;
+    private String flag;
+    private String action;
+    private String createdDate;
+    private String createdWho;
+    private String lastUpdate;
+    private String lastUpdateWho;
+    private String stDate;
+    private String kategori;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNamaObat() {
+        return namaObat;
+    }
+
+    public void setNamaObat(String namaObat) {
+        this.namaObat = namaObat;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getCaraPemberian() {
+        return caraPemberian;
+    }
+
+    public void setCaraPemberian(String caraPemberian) {
+        this.caraPemberian = caraPemberian;
+    }
+
+    public String getSkinTes() {
+        return skinTes;
+    }
+
+    public void setSkinTes(String skinTes) {
+        this.skinTes = skinTes;
+    }
+
+    public String getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(String waktu) {
+        this.waktu = waktu;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public String getNoCheckup() {
+        return noCheckup;
+    }
+
+    public void setNoCheckup(String noCheckup) {
+        this.noCheckup = noCheckup;
+    }
+
+    public String getIdDetailCheckup() {
+        return idDetailCheckup;
+    }
+
+    public void setIdDetailCheckup(String idDetailCheckup) {
+        this.idDetailCheckup = idDetailCheckup;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedWho() {
+        return createdWho;
+    }
+
+    public void setCreatedWho(String createdWho) {
+        this.createdWho = createdWho;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastUpdateWho() {
+        return lastUpdateWho;
+    }
+
+    public void setLastUpdateWho(String lastUpdateWho) {
+        this.lastUpdateWho = lastUpdateWho;
+    }
+
+    public String getStDate() {
+        return stDate;
+    }
+
+    public void setStDate(String stDate) {
+        this.stDate = stDate;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+}
