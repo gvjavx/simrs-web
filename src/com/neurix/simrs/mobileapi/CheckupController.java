a548a1582d255d0a85d9c2f7d6a24442e6ff2207
Mohammad Chaliffilardhy Syaifuddin <ardhymcs@gmail.com>
2020-03-08 20:39:56 +0700

    on progress rawat jalan api

--- src/com/neurix/simrs/mobileapi/CheckupController.java
+++ src/com/neurix/simrs/mobileapi/CheckupController.java
@@ -3,6 +3,10 @@ package com.neurix.simrs.mobileapi;
 import com.neurix.common.exception.GeneralBOException;
 import com.neurix.common.util.CommonUtil;
 import com.neurix.simrs.mobileapi.model.CheckupMobile;
+import com.neurix.simrs.mobileapi.model.HeaderCheckupMobile;
+import com.neurix.simrs.mobileapi.model.HeaderDetailCheckupMobile;
+import com.neurix.simrs.transaksi.checkup.bo.CheckupBo;
+import com.neurix.simrs.transaksi.checkup.model.HeaderCheckup;
 import com.neurix.simrs.transaksi.checkupdetail.bo.CheckupDetailBo;
 import com.neurix.simrs.transaksi.checkupdetail.model.HeaderDetailCheckup;
 import com.opensymphony.xwork2.ModelDriven;
@@ -10,9 +14,8 @@ import org.apache.log4j.Logger;
 import org.apache.struts2.rest.DefaultHttpHeaders;
 import org.apache.struts2.rest.HttpHeaders;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 
 /**
  * @author gondok
@@ -23,8 +26,12 @@ public class CheckupController implements ModelDriven<Object> {
     private static final transient Logger logger = Logger.getLogger(CheckupController.class);
     private CheckupMobile model = new CheckupMobile();
     private Collection<CheckupMobile> listOfCheckup = new ArrayList<>();
+    private Collection<HeaderDetailCheckupMobile> listOfHeaderCheckup = new ArrayList<>();
     private CheckupDetailBo checkupDetailBoProxy;
+    private CheckupBo checkupBoProxy;
 
+
+    private String idDetailCheckup;
     private String idPasien;
     private String nama;
     private String idPoli;
@@ -32,6 +39,49 @@ public class CheckupController implements ModelDriven<Object> {
     private String tglMasuk;
     private String branchId;
     private String action;
+
+    private File fileUploadTtd;
+    private String fileNameTtd;
+
+    public String getFileNameTtd() {
+        return fileNameTtd;
+    }
+
+    public void setFileNameTtd(String fileNameTtd) {
+        this.fileNameTtd = fileNameTtd;
+    }
+
+    public Collection<HeaderDetailCheckupMobile> getListOfHeaderCheckup() {
+        return listOfHeaderCheckup;
+    }
+
+    public void setListOfHeaderCheckup(Collection<HeaderDetailCheckupMobile> listOfHeaderCheckup) {
+        this.listOfHeaderCheckup = listOfHeaderCheckup;
+    }
+
+    public File getFileUploadTtd() {
+        return fileUploadTtd;
+    }
+
+    public void setFileUploadTtd(File fileUploadTtd) {
+        this.fileUploadTtd = fileUploadTtd;
+    }
+
+    public CheckupBo getCheckupBoProxy() {
+        return checkupBoProxy;
+    }
+
+    public void setCheckupBoProxy(CheckupBo checkupBoProxy) {
+        this.checkupBoProxy = checkupBoProxy;
+    }
+
+    public String getIdDetailCheckup() {
+        return idDetailCheckup;
+    }
+
+    public void setIdDetailCheckup(String idDetailCheckup) {
+        this.idDetailCheckup = idDetailCheckup;
+    }
 
     public String getBranchId() {
         return branchId;
@@ -115,42 +165,104 @@ public class CheckupController implements ModelDriven<Object> {
 
     @Override
     public Object getModel() {
-        return (listOfCheckup != null ? listOfCheckup : model);
+        switch (action) {
+            case "search":
+                return listOfCheckup;
+            case "dataPasien":
+                return listOfHeaderCheckup;
+            default:
+                return model;
+        }
     }
 
     public HttpHeaders create() {
         logger.info("[CheckupController.create] start process POST / <<<");
 
-        List<HeaderDetailCheckup> result = new ArrayList<>();
-
-        HeaderDetailCheckup bean = new HeaderDetailCheckup();
-        bean.setIdPasien(idPasien);
-        bean.setNamaPasien(nama);
-        bean.setIdPelayanan(idPoli);
-        bean.setStatusPeriksa(idStatusPasien);
-        bean.setTglCekup(CommonUtil.convertStringToDate(tglMasuk));
-        bean.setBranchId(branchId);
-
         if (action.equalsIgnoreCase("search")) {
+            List<HeaderDetailCheckup> result = new ArrayList<>();
+
+            HeaderDetailCheckup bean = new HeaderDetailCheckup();
+            bean.setIdPasien(idPasien);
+            bean.setNamaPasien(nama);
+            bean.setIdPelayanan(idPoli);
+            bean.setStatusPeriksa(idStatusPasien);
+            bean.setTglCekup(CommonUtil.convertStringToDate(tglMasuk));
+            bean.setBranchId(branchId);
+            bean.setFlag("Y");
+
             try {
                result = checkupDetailBoProxy.getSearchRawatJalan(bean);
             } catch (GeneralBOException e) {
                 logger.error("CheckupController.create] Error when get rawat jalan",e);
             }
-        }
 
-        if (result != null & result.size() > 0) {
-            for (HeaderDetailCheckup item : result) {
-                CheckupMobile checkupMobile = new CheckupMobile();
-                checkupMobile.setNoCheckup(item.getNoCheckup());
-                checkupMobile.setIdPasien(item.getIdPasien());
-                checkupMobile.setNamaPasien(item.getNamaPasien());
-                checkupMobile.setAlamat(item.getAlamat());
-                checkupMobile.setStatusPeriksa(item.getStatusPeriksa());
+            if (result != null & result.size() > 0) {
+                for (HeaderDetailCheckup item : result) {
+                    CheckupMobile checkupMobile = new CheckupMobile();
+                    checkupMobile.setIdDetailCheckup(item.getIdDetailCheckup());
+                    checkupMobile.setNamaPelayanan(item.getNamaPelayanan());
+                    checkupMobile.setJenisPeriksaPasien(item.getJenisPeriksaPasien());
+                    checkupMobile.setNoCheckup(item.getNoCheckup());
+                    checkupMobile.setIdPasien(item.getIdPasien());
+                    checkupMobile.setNamaPasien(item.getNamaPasien());
+                    checkupMobile.setAlamat(item.getAlamat());
+                    checkupMobile.setStatusPeriksa(item.getStatusPeriksa());
+                    checkupMobile.setStatusBayar(item.getStatusBayar());
 
 
-                listOfCheckup.add(checkupMobile);
+
+                    listOfCheckup.add(checkupMobile);
+
+
+                }
             }
+        } else if (action.equalsIgnoreCase("dataPasien")){
+
+            HeaderCheckup result = new HeaderCheckup();
+
+            try {
+               result = checkupBoProxy.getDataDetailPasien(idDetailCheckup);
+            } catch (GeneralBOException e){
+                logger.error("CheckupController.create] Error when get rawat jalan",e);
+
+            }
+
+            if (result != null) {
+               HeaderDetailCheckupMobile headerDetailCheckupMobile = new HeaderDetailCheckupMobile();
+               headerDetailCheckupMobile.setNoCheckup(result.getNoCheckup());
+               headerDetailCheckupMobile.setIdPasien(result.getIdPasien());
+               headerDetailCheckupMobile.setNamaPasien(result.getNama());
+               headerDetailCheckupMobile.setJenisKelamin(result.getJenisKelamin());
+               headerDetailCheckupMobile.setNik(result.getNoKtp());
+               headerDetailCheckupMobile.setTempatLahir(result.getTempatLahir());
+               headerDetailCheckupMobile.setIdJenisPeriksaPasien(result.getIdJenisPeriksaPasien());
+               headerDetailCheckupMobile.setIdDetailCheckup(result.getIdDetailCheckup());
+               headerDetailCheckupMobile.setNamaPelayanan(result.getNamaPelayanan());
+               headerDetailCheckupMobile.setNoSep(result.getNoSep());
+               headerDetailCheckupMobile.setTglLahir(result.getStTglLahir());
+
+               listOfHeaderCheckup.add(headerDetailCheckupMobile);
+            }
+        } else if (action.equalsIgnoreCase("uploadTtd")){
+
+            List<HeaderDetailCheckup> result = new ArrayList<>();
+
+            HeaderDetailCheckup bean = new HeaderDetailCheckup();
+            bean.setIdDetailCheckup(idDetailCheckup);
+
+            try {
+                result = checkupDetailBoProxy.getByCriteria(bean);
+
+            } catch (GeneralBOException e){
+                logger.error("CheckupController.create] Error when get checkup detail by criteria",e);
+            }
+
+            if (result != null && result.size() > 0){
+                if (fileUploadTtd != null) {
+                    //TODO
+                }
+            }
+
         }
 
         logger.info("[CheckupController.create] end process POST / <<<");
