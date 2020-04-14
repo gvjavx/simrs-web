4ee2a6ca94d7a47fbb79b3151fba4357aca708b5
Sigit Wahyu Wibowo <sigitwahyu05@gmail.com>
2020-04-14 10:32:50 +0700

    penambahkan rawat BE tindakan transitoris

--- src/com/neurix/simrs/transaksi/riwayattindakan/bo/RiwayatTindakanBo.java
+++ src/com/neurix/simrs/transaksi/riwayattindakan/bo/RiwayatTindakanBo.java
@@ -4,6 +4,7 @@ import com.neurix.common.exception.GeneralBOException;
 import com.neurix.simrs.transaksi.riwayattindakan.model.ItSimrsTindakanTransitorisEntity;
 import com.neurix.simrs.transaksi.riwayattindakan.model.RiwayatTindakan;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface RiwayatTindakanBo {
@@ -12,6 +13,6 @@ public interface RiwayatTindakanBo {
     public void saveAdd(RiwayatTindakan bean) throws GeneralBOException;
     public void saveEdit(RiwayatTindakan bean) throws GeneralBOException;
     public List<RiwayatTindakan> cekTodayTarifKamar(String idDetail) throws GeneralBOException;
-    public void saveTindakanTransitoris(String  idDetailCheckup) throws GeneralBOException;
+    public void saveTindakanTransitoris(String  idDetailCheckup, Timestamp time, String user) throws GeneralBOException;
 
 }
