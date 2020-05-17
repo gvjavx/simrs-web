6567760718ca087d316b61d53e7615a03a89a694
Sigit Wahyu Wibowo <sigitwahyu05@gmail.com>
2020-05-17 09:05:41 +0700

    perbaikan

--- src/com/neurix/simrs/transaksi/permintaanvendor/bo/Impl/PermintaanVendorBoImpl.java
+++ src/com/neurix/simrs/transaksi/permintaanvendor/bo/Impl/PermintaanVendorBoImpl.java
@@ -1,6 +1,9 @@
 package com.neurix.simrs.transaksi.permintaanvendor.bo.Impl;
 
 import com.google.gson.Gson;
+import com.neurix.authorization.company.dao.BranchDao;
+import com.neurix.authorization.company.model.ImBranches;
+import com.neurix.authorization.company.model.ImBranchesPK;
 import com.neurix.common.exception.GeneralBOException;
 import com.neurix.common.util.CommonUtil;
 import com.neurix.simrs.master.obat.dao.ObatDao;
@@ -19,7 +22,9 @@ import com.neurix.simrs.transaksi.permintaanvendor.dao.PermintaanVendorDao;
 import com.neurix.simrs.transaksi.permintaanvendor.dao.TempObatGejalaDao;
 import com.neurix.simrs.transaksi.permintaanvendor.model.*;
 import com.neurix.simrs.transaksi.riwayatbarang.dao.RiwayatBarangDao;
+import com.neurix.simrs.transaksi.riwayatbarang.dao.TransaksiStokDao;
 import com.neurix.simrs.transaksi.riwayatbarang.model.ItSimrsRiwayatBarangMasukEntity;
+import com.neurix.simrs.transaksi.riwayatbarang.model.ItSimrsTransaksiStokEntity;
 import com.neurix.simrs.transaksi.transaksiobat.dao.ApprovalTransaksiObatDao;
 import com.neurix.simrs.transaksi.transaksiobat.dao.TransaksiObatDetailBatchDao;
 import com.neurix.simrs.transaksi.transaksiobat.dao.TransaksiObatDetailDao;
@@ -58,6 +63,8 @@ public class PermintaanVendorBoImpl implements PermintaanVendorBo {
     private VendorDao vendorDao;
     private ObatGejalaDao obatGejalaDao;
     private RiwayatBarangDao riwayatBarangDao;
+    private TransaksiStokDao transaksiStokDao;
+    private BranchDao branchDao;
 
     @Override
     public List<PermintaanVendor> getByCriteria(PermintaanVendor bean) throws GeneralBOException {
@@ -735,6 +742,7 @@ public class PermintaanVendorBoImpl implements PermintaanVendorBo {
                                 obatDetail.setBranchId(bean.getBranchId());
                                 obatDetail.setTipeObat(batchEntity.getFlagObatBpjs());
                                 obatDetail.setNetto(batchEntity.getNetto());
+                                obatDetail.setIdVendor(bean.getIdVendor());
                                 //update stock and new harga rata-rata
                                 updateAddStockGudang(obatDetail);
                             }
@@ -971,7 +979,7 @@ public class PermintaanVendorBoImpl implements PermintaanVendorBo {
         }
 
         updateAllNewAverageHargaByObatId(bean.getIdObat(), newObatEntity.getAverageHargaBox(), newObatEntity.getAverageHargaLembar(), newObatEntity.getAverageHargaBiji());
-        saveToRiwayatBarangMasuk(obatEntity, "");
+        saveToRiwayatBarangMasuk(obatEntity, bean.getIdVendor());
 
         logger.info("[PermintaanVendorBoImpl.updateAddStockGudang] END <<<");
     }
@@ -981,19 +989,28 @@ public class PermintaanVendorBoImpl implements PermintaanVendorBo {
 
         if (obatEntity != null){
 
+            ImBranchesPK branchesPK = new ImBranchesPK();
+            branchesPK.setId(obatEntity.getBranchId());
+
+            ImBranches branches = branchDao.getById("primaryKey.id", branchesPK);
+            String branchName = "";
+            if (branches != null){
+                branchName = branches.getBranchName();
+            }
+
+            String vendorName = "";
+            if (idVendor != null && !"".equalsIgnoreCase(idVendor)){
+                ImSimrsVendorEntity vendorEntity = vendorDao.getById("idVendor", idVendor);
+                if (vendorEntity != null){
+                    vendorName = vendorEntity.getNamaVendor();
+                }
+            }
+
             BigInteger cons = obatEntity.getBijiPerLembar().multiply(obatEntity.getLembarPerBox());
             BigInteger boxToBiji = obatEntity.getQtyBox().multiply(cons);
             BigInteger lembarToBiji = obatEntity.getQtyLembar().multiply(obatEntity.getBijiPerLembar());
             BigInteger qty = obatEntity.getQtyBiji().add(lembarToBiji).add(boxToBiji);
-
-            BigInteger qtyStock = new BigInteger(String.valueOf(0));
-            Obat stockObat = obatDao.getSumStockObatGudangById(obatEntity.getIdObat(), "");
-            if (stockObat != null){
-                BigInteger stockBox = stockObat.getQtyBox().multiply(cons);
-                BigInteger stockLembar = stockObat.getQtyLembar().multiply(obatEntity.getBijiPerLembar());
-                BigInteger stockBiji = stockObat.getQtyBiji().add(stockBox).add(stockLembar);
-                qtyStock = stockBiji;
-            }
+            BigDecimal hargaBarang = obatEntity.getHargaTerakhir().divide(new BigDecimal(cons) ,2, RoundingMode.HALF_UP);
 
             java.util.Date now = new java.util.Date();
             SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd");
@@ -1001,24 +1018,23 @@ public class PermintaanVendorBoImpl implements PermintaanVendorBo {
             String seq = riwayatBarangDao.getNextSeq();
             String idBarangMasuk = "RB"+ obatEntity.getBranchId() + f.format(now) + seq;
 
-            ItSimrsRiwayatBarangMasukEntity barangMasukEntity = new ItSimrsRiwayatBarangMasukEntity();
-            barangMasukEntity.setIdBarangMasuk(idBarangMasuk);
-            barangMasukEntity.setIdBarang(obatEntity.getIdBarang());
-            barangMasukEntity.setIdVendor(idVendor);
-            barangMasukEntity.setBranchId(obatEntity.getBranchId());
-            barangMasukEntity.setHargaTerakhir(obatEntity.getHargaTerakhir().divide(new BigDecimal(cons) ,2, RoundingMode.HALF_UP));
-            barangMasukEntity.setQty(qty);
-            barangMasukEntity.setAverageHarga(obatEntity.getAverageHargaBiji());
-            barangMasukEntity.setFlag(obatEntity.getFlag());
-            barangMasukEntity.setAction(obatEntity.getAction());
-            barangMasukEntity.setCreatedDate(obatEntity.getCreatedDate());
-            barangMasukEntity.setLastUpdate(obatEntity.getLastUpdate());
-            barangMasukEntity.setCreatedWho(obatEntity.getCreatedWho());
-            barangMasukEntity.setLastUpdateWho(obatEntity.getLastUpdateWho());
-            barangMasukEntity.setStock(qtyStock.subtract(qty));
+            ItSimrsTransaksiStokEntity transaksiStokEntity = new ItSimrsTransaksiStokEntity();
+            transaksiStokEntity.setIdTransaksi(idBarangMasuk);
+            transaksiStokEntity.setIdObat(obatEntity.getIdObat());
+            transaksiStokEntity.setKeterangan("Barang Masuk Pada"+ branchName + " Nama Barang " + obatEntity.getNamaObat() + "Dari Vendor : " + vendorName);
+            transaksiStokEntity.setTipe("D");
+            transaksiStokEntity.setBranchId(obatEntity.getBranchId());
+            transaksiStokEntity.setQty(qty);
+            transaksiStokEntity.setTotal(hargaBarang);
+            transaksiStokEntity.setSubTotal(hargaBarang.multiply(new BigDecimal(qty)));
+            transaksiStokEntity.setRegisteredDate(new Date(obatEntity.getLastUpdate().getTime()));
+            transaksiStokEntity.setCreatedDate(obatEntity.getLastUpdate());
+            transaksiStokEntity.setCreatedWho(obatEntity.getLastUpdateWho());
+            transaksiStokEntity.setLastUpdate(obatEntity.getLastUpdate());
+            transaksiStokEntity.setLastUpdateWho(obatEntity.getLastUpdateWho());
 
             try {
-                riwayatBarangDao.addAndSave(barangMasukEntity);
+                transaksiStokDao.addAndSave(transaksiStokEntity);
             } catch (HibernateException e){
                 logger.error("[PermintaanVendorBoImpl.saveToRiwayatBarangMasuk] ERROR.", e);
                 throw new GeneralBOException("[PermintaanVendorBoImpl.saveToRiwayatBarangMasuk] ERROR." + e.getMessage());
@@ -1721,5 +1737,13 @@ public class PermintaanVendorBoImpl implements PermintaanVendorBo {
 
     public void setRiwayatBarangDao(RiwayatBarangDao riwayatBarangDao) {
         this.riwayatBarangDao = riwayatBarangDao;
+    }
+
+    public void setTransaksiStokDao(TransaksiStokDao transaksiStokDao) {
+        this.transaksiStokDao = transaksiStokDao;
+    }
+
+    public void setBranchDao(BranchDao branchDao) {
+        this.branchDao = branchDao;
     }
 }
---
+++ src/com/neurix/simrs/transaksi/riwayatbarang/model/ItSimrsTransaksiStokEntity.java
@@ -0,0 +1,149 @@
+package com.neurix.simrs.transaksi.riwayatbarang.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+/**
+ * Created by reza on 14/05/20.
+ */
+public class ItSimrsTransaksiStokEntity implements Serializable {
+
+    private String idTransaksi;
+    private String idObat;
+    private String keterangan;
+    private String tipe;
+    private String branchId;
+    private BigInteger qty;
+    private BigDecimal total;
+    private BigDecimal subTotal;
+    private Date registeredDate;
+    private Timestamp createdDate;
+    private String createdWho;
+    private Timestamp lastUpdate;
+    private String lastUpdateWho;
+    private String idVendor;
+    private String idBarang;
+
+    public String getIdTransaksi() {
+        return idTransaksi;
+    }
+
+    public void setIdTransaksi(String idTransaksi) {
+        this.idTransaksi = idTransaksi;
+    }
+
+    public String getIdObat() {
+        return idObat;
+    }
+
+    public void setIdObat(String idObat) {
+        this.idObat = idObat;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public BigInteger getQty() {
+        return qty;
+    }
+
+    public void setQty(BigInteger qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public BigDecimal getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(BigDecimal subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public Date getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(Date registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedWho() {
+        return createdWho;
+    }
+
+    public void setCreatedWho(String createdWho) {
+        this.createdWho = createdWho;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastUpdateWho() {
+        return lastUpdateWho;
+    }
+
+    public void setLastUpdateWho(String lastUpdateWho) {
+        this.lastUpdateWho = lastUpdateWho;
+    }
+
+    public String getIdVendor() {
+        return idVendor;
+    }
+
+    public void setIdVendor(String idVendor) {
+        this.idVendor = idVendor;
+    }
+
+    public String getIdBarang() {
+        return idBarang;
+    }
+
+    public void setIdBarang(String idBarang) {
+        this.idBarang = idBarang;
+    }
+}
