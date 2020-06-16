3626bd7d8d0f52de76acbc16153ac35ee475262d
Mohammad Chaliffilardhy Syaifuddin <ardhymcs@gmail.com>
2020-06-16 16:09:54 +0700

    progress resep telemedic

---
+++ src/com/neurix/simrs/transaksi/reseponline/bo/ResepOnlineBo.java
@@ -0,0 +1,11 @@
+package com.neurix.simrs.transaksi.reseponline.bo;
+
+import com.neurix.common.bo.GeneralBo;
+
+/**
+ * @author gondok
+ * Tuesday, 16/06/20 16:07
+ */
+public interface ResepOnlineBo extends GeneralBo {
+
+}
---
+++ src/com/neurix/simrs/transaksi/reseponline/bo/impl/ResepOnlineBoImpl.java
@@ -0,0 +1,16 @@
+package com.neurix.simrs.transaksi.reseponline.bo.impl;
+
+import com.neurix.common.exception.GeneralBOException;
+import com.neurix.simrs.transaksi.reseponline.bo.ResepOnlineBo;
+
+/**
+ * @author gondok
+ * Tuesday, 16/06/20 16:08
+ */
+public class ResepOnlineBoImpl implements ResepOnlineBo {
+
+    @Override
+    public Long saveErrorMessage(String message, String moduleMethod) throws GeneralBOException {
+        return null;
+    }
+}
