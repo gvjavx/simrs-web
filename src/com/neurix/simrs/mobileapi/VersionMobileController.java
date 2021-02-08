30402a60e49da3c07be72c2599ec596fea898da2
chalippi <ardhymcs@gmail.com>
2021-02-08 13:06:18 +0700

    review sodiq

--- src/com/neurix/simrs/mobileapi/VersionMobileController.java
+++ src/com/neurix/simrs/mobileapi/VersionMobileController.java
@@ -2,8 +2,8 @@ package com.neurix.simrs.mobileapi;
 
 import com.neurix.common.exception.GeneralBOException;
 import com.neurix.common.util.CommonUtil;
-import com.neurix.simrs.master.license.bo.VersionMobileBo;
-import com.neurix.simrs.master.license.model.VersionMobile;
+//import com.neurix.simrs.master.license.bo.VersionMobileBo;
+//import com.neurix.simrs.master.license.model.VersionMobile;
 import com.neurix.simrs.mobileapi.model.VersionMobileObj;
 import com.opensymphony.xwork2.ModelDriven;
 import org.apache.log4j.Logger;
@@ -18,7 +18,7 @@ public class VersionMobileController implements ModelDriven<Object> {
     private static final transient Logger logger = Logger.getLogger(VersionMobileController.class);
     private VersionMobileObj model = new VersionMobileObj();
     private Collection<VersionMobileObj> listOfVersionMobile = new ArrayList();
-    private VersionMobileBo versionMobileBoProxy;
+//    private VersionMobileBo versionMobileBoProxy;
 
     String action;
     String idVersionMobile;
@@ -66,9 +66,9 @@ public class VersionMobileController implements ModelDriven<Object> {
         this.os = os;
     }
 
-    public void setVersionMobileBoProxy(VersionMobileBo versionMobileBoProxy) {
-        this.versionMobileBoProxy = versionMobileBoProxy;
-    }
+//    public void setVersionMobileBoProxy(VersionMobileBo versionMobileBoProxy) {
+//        this.versionMobileBoProxy = versionMobileBoProxy;
+//    }
 
     @Override
     public Object getModel() {
@@ -78,68 +78,68 @@ public class VersionMobileController implements ModelDriven<Object> {
     public HttpHeaders create() {
         logger.info("[VersionMobile.create] start process POST / <<<");
 
-        if (action.equalsIgnoreCase("getByCriteria"))
-            getByCriteria();
-        if (action.equalsIgnoreCase("saveAdd"))
-            saveAdd();
+//        if (action.equalsIgnoreCase("getByCriteria"))
+//            getByCriteria();
+//        if (action.equalsIgnoreCase("saveAdd"))
+//            saveAdd();
 
         logger.info("[VersionMobile.create] start process POST / <<<");
         return new DefaultHttpHeaders("index").disableCaching();
     }
 
-    private void getByCriteria() {
-        List<VersionMobile> result = new ArrayList<>();
-        listOfVersionMobile = new ArrayList<>();
-
-        VersionMobile bean = new VersionMobile();
-        bean.setIdVersionMobile(idVersionMobile);
-        bean.setNamaVersion(namaVersion);
-        bean.setFlag(flag);
-        bean.setOs(os);
-
-        try {
-           result = versionMobileBoProxy.getByCriteria(bean);
-        } catch (GeneralBOException e) {
-            logger.error("[VersionMobile.getByCriteria] Error when search getByCriteria, " + e.getMessage());
-            throw new GeneralBOException("[LicenseAction.searchLicense] Error when search license, " + e.getMessage());
-        }
-
-        if (result.size() > 0) {
-            for (VersionMobile item : result) {
-                VersionMobileObj versionMobileObj = new VersionMobileObj();
-                versionMobileObj.setIdVersionMobile(item.getIdVersionMobile());
-                versionMobileObj.setNamaVersion(item.getNamaVersion());
-                versionMobileObj.setOs(item.getOs());
-                versionMobileObj.setFlag(item.getFlag());
-                versionMobileObj.setAction(item.getAction());
-                versionMobileObj.setCreatedDate(item.getCreatedDate().toLocaleString());
-                versionMobileObj.setLastUpdate(item.getLastUpdate().toLocaleString());
-                versionMobileObj.setCreatedWho(item.getCreatedWho());
-                versionMobileObj.setLastUpdateWho(item.getLastUpdateWho());
-
-                listOfVersionMobile.add(versionMobileObj);
-            }
-        }
-    }
-
-    private void saveAdd() {
-
-        VersionMobile bean = new VersionMobile();
-        bean.setNamaVersion(namaVersion);
-        bean.setOs(os);
-        bean.setFlag("Y");
-        bean.setAction("C");
-        bean.setLastUpdate(CommonUtil.getCurrentDateTimes());
-        bean.setLastUpdateWho("admin");
-        bean.setCreatedDate(CommonUtil.getCurrentDateTimes());
-        bean.setCreatedWho("admin");
-
-        try {
-            versionMobileBoProxy.saveAdd(bean);
-            model.setMessage("success");
-        } catch (GeneralBOException e) {
-            logger.error("[VersionMobileController.saveAdd] Error when saveAdd, " + e.getMessage());
-            throw new GeneralBOException("[VersionMobileController.saveAdd] Error saveAdd, " + e.getMessage());
-        }
-    }
+//    private void getByCriteria() {
+//        List<VersionMobile> result = new ArrayList<>();
+//        listOfVersionMobile = new ArrayList<>();
+//
+//        VersionMobile bean = new VersionMobile();
+//        bean.setIdVersionMobile(idVersionMobile);
+//        bean.setNamaVersion(namaVersion);
+//        bean.setFlag(flag);
+//        bean.setOs(os);
+//
+//        try {
+//           result = versionMobileBoProxy.getByCriteria(bean);
+//        } catch (GeneralBOException e) {
+//            logger.error("[VersionMobile.getByCriteria] Error when search getByCriteria, " + e.getMessage());
+//            throw new GeneralBOException("[LicenseAction.searchLicense] Error when search license, " + e.getMessage());
+//        }
+//
+//        if (result.size() > 0) {
+//            for (VersionMobile item : result) {
+//                VersionMobileObj versionMobileObj = new VersionMobileObj();
+//                versionMobileObj.setIdVersionMobile(item.getIdVersionMobile());
+//                versionMobileObj.setNamaVersion(item.getNamaVersion());
+//                versionMobileObj.setOs(item.getOs());
+//                versionMobileObj.setFlag(item.getFlag());
+//                versionMobileObj.setAction(item.getAction());
+//                versionMobileObj.setCreatedDate(item.getCreatedDate().toLocaleString());
+//                versionMobileObj.setLastUpdate(item.getLastUpdate().toLocaleString());
+//                versionMobileObj.setCreatedWho(item.getCreatedWho());
+//                versionMobileObj.setLastUpdateWho(item.getLastUpdateWho());
+//
+//                listOfVersionMobile.add(versionMobileObj);
+//            }
+//        }
+//    }
+//
+//    private void saveAdd() {
+//
+//        VersionMobile bean = new VersionMobile();
+//        bean.setNamaVersion(namaVersion);
+//        bean.setOs(os);
+//        bean.setFlag("Y");
+//        bean.setAction("C");
+//        bean.setLastUpdate(CommonUtil.getCurrentDateTimes());
+//        bean.setLastUpdateWho("admin");
+//        bean.setCreatedDate(CommonUtil.getCurrentDateTimes());
+//        bean.setCreatedWho("admin");
+//
+//        try {
+//            versionMobileBoProxy.saveAdd(bean);
+//            model.setMessage("success");
+//        } catch (GeneralBOException e) {
+//            logger.error("[VersionMobileController.saveAdd] Error when saveAdd, " + e.getMessage());
+//            throw new GeneralBOException("[VersionMobileController.saveAdd] Error saveAdd, " + e.getMessage());
+//        }
+//    }
 }
