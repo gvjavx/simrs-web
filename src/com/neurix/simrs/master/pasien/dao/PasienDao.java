f40ee83948346fffec80ccd046f14fcfbb756ae6
Sigit Wahyu Wibowo <sigitwahyu05@gmail.com>
2021-02-09 18:24:04 +0700

    perbaikan search pasien

--- src/com/neurix/simrs/master/pasien/dao/PasienDao.java
+++ src/com/neurix/simrs/master/pasien/dao/PasienDao.java
@@ -383,7 +383,7 @@ public class PasienDao extends GenericDao<ImSimrsPasienEntity, String> {
                 pasien.setAgama(stNullEscape(obj[10]));
                 pasien.setProfesi(stNullEscape(obj[11]));
                 pasien.setNoTelp(stNullEscape(obj[12]));
-                pasien.setUrlKtp(stNullEscape(obj[13]));
+                pasien.setUrlKtp(CommonConstant.EXTERNAL_IMG_URI + CommonConstant.RESOURCE_PATH_KTP_PASIEN + stNullEscape(obj[13]));
                 pasien.setFlag(stNullEscape(obj[14]));
                 pasien.setAction(stNullEscape(obj[15]));
                 pasien.setCreatedDate(obj[16] == null ? null : (Timestamp) obj[16]);
@@ -399,7 +399,158 @@ public class PasienDao extends GenericDao<ImSimrsPasienEntity, String> {
                 pasien.setKecamatan(stNullEscape(obj[26]));
                 pasien.setKota(stNullEscape(obj[27]));
                 pasien.setProvinsi(stNullEscape(obj[28]));
-                pasien.setDisabledFingerData(foundFinggerData(obj[29]));
+                pasien.setDisabledFingerData(foundBoolean(obj[29]));
+                pasienList.add(pasien);
+            }
+        }
+        return pasienList;
+    }
+
+    public List<Pasien> getSearchForTransaksi(Pasien param){
+
+        String where = "";
+        if (param.getIdPasien() != null && !"".equalsIgnoreCase(param.getIdPasien()))
+            where += "AND ps.id_pasien = '"+param.getIdPasien()+"' \n";
+        if (param.getNama() != null && !"".equalsIgnoreCase(param.getNama()))
+            where += "AND ps.nama ILIKE '%"+param.getNama()+"%' \n";
+        if (param.getDesaId() != null && !"".equalsIgnoreCase(param.getDesaId()))
+            where += "AND ps.desa_id = '"+param.getDesaId()+"' \n";
+        if (param.getNoKtp() != null && !"".equalsIgnoreCase(param.getNoKtp()))
+            where += "AND ps.no_ktp = '"+param.getNoKtp()+"' \n";
+        if (param.getNoBpjs() != null && !"".equalsIgnoreCase(param.getNoBpjs()))
+            where += "AND ps.no_bpjs = '"+param.getNoBpjs()+"' \n";
+
+        String SQL = "SELECT\n" +
+                "ps.id_pasien,\n" +
+                "ps.nama,\n" +
+                "ps.jenis_kelamin,\n" +
+                "ps.no_ktp,\n" +
+                "ps.no_bpjs,\n" +
+                "ps.tempat_lahir,\n" +
+                "ps.tgl_lahir,\n" +
+                "ps.desa_id,\n" +
+                "ps.jalan,\n" +
+                "ps.suku,\n" +
+                "ps.agama,\n" +
+                "ps.profesi,\n" +
+                "ps.no_telp,\n" +
+                "ps.url_ktp,\n" +
+                "ps.flag,\n" +
+                "ps.action,\n" +
+                "ps.created_date,\n" +
+                "ps.created_who,\n" +
+                "ps.last_update,\n" +
+                "ps.last_update_who,\n" +
+                "ps.email,\n" +
+                "ps.password,\n" +
+                "ps.pendidikan,\n" +
+                "ps.status_perkawinan,\n" +
+                "ps.flag_login,\n" +
+                "pss.desa_name,\n" +
+                "pss.kecamatan_name,\n" +
+                "pss.kota_name,\n" +
+                "pss.provinsi_name,\n" +
+                "pss.id_finger_data,\n" +
+                "pss.id_pelayanan,\n" +
+                "pss.no_checkup_ulang,\n" +
+                "pss.id_detail_checkup,\n" +
+                "pss.is_order_lab,\n" +
+                "pss.pasien_lama\n" +
+                "FROM im_simrs_pasien ps\n" +
+                "INNER JOIN (\n" +
+                "\tSELECT\n" +
+                "\tps.id_pasien,\n" +
+                "\tds.desa_name,\n" +
+                "\tkc.kecamatan_name,\n" +
+                "\tkt.kota_name,\n" +
+                "\tpv.provinsi_name,\n" +
+                "\tfd.id_finger_data,\n" +
+                "\tdc.id_pelayanan,\n" +
+                "\tdc.no_checkup_ulang,\n" +
+                "\tdc.id_detail_checkup,\n" +
+                "\tdc.is_order_lab,\n" +
+                "\thd.id_pasien as pasien_lama\n" +
+                "\tFROM im_simrs_pasien ps\n" +
+                "\tLEFT JOIN (SELECT desa_id, desa_name, kecamatan_id FROM im_hris_desa WHERE flag = 'Y') ds ON ds.desa_id = CAST( ps.desa_id AS VARCHAR )\n" +
+                "\tINNER JOIN (SELECT kecamatan_id, kecamatan_name, kota_id FROM im_hris_kecamatan WHERE flag = 'Y') kc ON kc.kecamatan_id = ds.kecamatan_id\n" +
+                "\tINNER JOIN (SELECT kota_id, kota_name, provinsi_id FROM im_hris_kota WHERE flag = 'Y') kt ON kt.kota_id = kc.kota_id\n" +
+                "\tINNER JOIN (SELECT provinsi_id, provinsi_name FROM im_hris_provinsi WHERE flag = 'Y') pv ON pv.provinsi_id = kt.provinsi_id\n" +
+                "\tLEFT JOIN (SELECT id_finger_data, id_pasien FROM im_simrs_finger_data WHERE flag = 'Y') fd ON fd.id_pasien = ps.id_pasien\n" +
+                "\tLEFT JOIN (SELECT id_pasien FROM it_simrs_header_checkup WHERE flag = 'Y' GROUP BY id_pasien) hd ON hd.id_pasien = ps.id_pasien\n" +
+                "\tLEFT JOIN ( \n" +
+                "\t\t-- mencari data detail checkup terakhir terhadap pasien as dc;\n" +
+                "\t\tSELECT \n" +
+                "\t\tb.id_detail_checkup,\n" +
+                "\t\ta.id_pasien,\n" +
+                "\t\tb.tgl_cekup,\n" +
+                "\t\tb.no_checkup_ulang,\n" +
+                "\t\tb.is_order_lab, \n" +
+                "\t\tb.id_pelayanan\n" +
+                "\t\tFROM it_simrs_header_detail_checkup b\n" +
+                "\t\tINNER JOIN \n" +
+                "\t\t(\n" +
+                "\t\t\tSELECT \n" +
+                "\t\t\tb.id_pasien,\n" +
+                "\t\t\ta.id_detail_checkup\n" +
+                "\t\t\tFROM (\n" +
+                "\t\t\t\tSELECT id_detail_checkup, b.id_pasien, a.created_date FROM it_simrs_header_detail_checkup a\n" +
+                "\t\t\t\tINNER JOIN (SELECT no_checkup, id_pasien FROM it_simrs_header_checkup) b ON b.no_checkup = a.no_checkup\n" +
+                "\t\t\t) a\n" +
+                "\t\t\tINNER JOIN \n" +
+                "\t\t\t(\n" +
+                "\t\t\t\tSELECT \n" +
+                "\t\t\t\ta.id_pasien,\n" +
+                "\t\t\t\tMAX(b.created_date) as created_date\n" +
+                "\t\t\t\tFROM (SELECT id_pasien, no_checkup FROM it_simrs_header_checkup WHERE tgl_keluar IS NOT NULL) a\n" +
+                "\t\t\t\tINNER JOIN (SELECT no_checkup, created_date FROM it_simrs_header_detail_checkup) b ON b.no_checkup = a.no_checkup\n" +
+                "\t\t\t\tGROUP BY a.id_pasien\n" +
+                "\t\t\t) b ON b.id_pasien = a.id_pasien AND b.created_date = a.created_date\n" +
+                "\t\t) a ON b.id_detail_checkup = a.id_detail_checkup\n" +
+                "\t\t-- end sub query\n" +
+                "\t) dc ON dc.id_pasien = ps.id_pasien\n" +
+                ") pss ON pss.id_pasien = ps.id_pasien" +
+                "WHERE ps.flag = 'Y' \n" + where;
+
+        List<Object[]> results = this.sessionFactory.getCurrentSession().createSQLQuery(SQL).list();
+        List<Pasien> pasienList = new ArrayList<>();
+        if (results.size() > 0){
+            for (Object[] obj : results){
+                Pasien pasien = new Pasien();
+                pasien.setIdPasien(obj[0].toString());
+                pasien.setNama(stNullEscape(obj[1]));
+                pasien.setJenisKelamin(stNullEscape(obj[2]));
+                pasien.setNoKtp(stNullEscape(obj[3]));
+                pasien.setNoBpjs(stNullEscape(obj[4]));
+                pasien.setTempatLahir(stNullEscape(obj[5]));
+                pasien.setTglLahir(stNullEscape(obj[6]));
+                pasien.setDesaId(stNullEscape(obj[7]));
+                pasien.setJalan(stNullEscape(obj[8]));
+                pasien.setSuku(stNullEscape(obj[9]));
+                pasien.setAgama(stNullEscape(obj[10]));
+                pasien.setProfesi(stNullEscape(obj[11]));
+                pasien.setNoTelp(stNullEscape(obj[12]));
+                pasien.setUrlKtp(CommonConstant.EXTERNAL_IMG_URI + CommonConstant.RESOURCE_PATH_KTP_PASIEN + stNullEscape(obj[13]));
+                pasien.setFlag(stNullEscape(obj[14]));
+                pasien.setAction(stNullEscape(obj[15]));
+                pasien.setCreatedDate(obj[16] == null ? null : (Timestamp) obj[16]);
+                pasien.setCreatedWho(stNullEscape(obj[17]));
+                pasien.setLastUpdate(obj[18] == null ? null : (Timestamp) obj[18]);
+                pasien.setLastUpdateWho(stNullEscape(obj[19]));
+                pasien.setEmail(stNullEscape(obj[20]));
+                pasien.setPassword(stNullEscape(obj[21]));
+                pasien.setPendidikan(stNullEscape(obj[22]));
+                pasien.setStatusPerkawinan(stNullEscape(obj[23]));
+                pasien.setFlagLogin(stNullEscape(obj[24]));
+                pasien.setDesa(stNullEscape(obj[25]));
+                pasien.setKecamatan(stNullEscape(obj[26]));
+                pasien.setKota(stNullEscape(obj[27]));
+                pasien.setProvinsi(stNullEscape(obj[28]));
+                pasien.setDisabledFingerData(foundBoolean(obj[29]));
+                pasien.setIdPelayanan(stNullEscape(30));
+                pasien.setNoCheckuoUlang(stNullEscape(31));
+                pasien.setIdLastDetailCheckup(stNullEscape(32));
+                pasien.setIsOrderLab(stNullEscape(33));
+                pasien.setIsPasienLama(foundBoolean(obj[34]));
                 pasienList.add(pasien);
             }
         }
@@ -410,7 +561,7 @@ public class PasienDao extends GenericDao<ImSimrsPasienEntity, String> {
         return obj == null ? "" : obj.toString();
     }
 
-    private Boolean foundFinggerData(Object obj){
+    private Boolean foundBoolean(Object obj){
         boolean found = obj != null;
         return found;
     }
