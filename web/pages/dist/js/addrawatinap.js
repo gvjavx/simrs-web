f2dddbfbd2e3f4bb34a025f44d1ebaade54798ab
Muhammad Sodiq <muhmmdsodiq@gmail.com>
2021-03-23 18:49:48 +0700

    okeh

--- web/pages/dist/js/addrawatinap.js
+++ web/pages/dist/js/addrawatinap.js
@@ -333,7 +333,7 @@ function selectKeterangan(idKtg) {
             $('#form-asesmen').show();
 
         } else if (idKtg == "rr") {
-            getKamar(null, 'ruang_recovery');
+            getKamar(null, 'rr');
             $('#label_kamar').text('Ruangan');
             $('#form-catatan').show();
             $('#form-kamar').show();
